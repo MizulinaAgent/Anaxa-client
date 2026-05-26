8ee3374bd7e9b10ac8803bba8ede461ec0210af5
Agusha <mikniksertan@gmail.com>
2026-05-27 01:47:44 +0300

    feat(theme): тёмная изумрудная цветовая схема

--- app/src/main/java/com/example/anaxa/ui/theme/Color.kt
+++ app/src/main/java/com/example/anaxa/ui/theme/Color.kt
@@ -2,10 +2,24 @@ package com.example.anaxa.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Background = Color(0xFF0A120D)
+val Surface = Color(0xFF0F1A14)
+val SurfaceVariant = Color(0xFF16261C)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val EmeraldDeep = Color(0xFF0C5E40)
+val EmeraldMid = Color(0xFF1E9E6E)
+val EmeraldBright = Color(0xFF34C78E)
+val EmeraldLight = Color(0xFF3FD89B)
+val EmeraldMint = Color(0xFF4FE6A8)
+
+val NeonEmerald = Color(0xFF2BE89A)
+val NeonBright = Color(0xFF5BF5AE)
+val NeonMint = Color(0xFF7BFFC9)
+
+val AquaCyan = Color(0xFF00E0C6)
+
+val TextPrimary = Color(0xFFECFFF6)
+val TextSecondary = Color(0xFFCFFFEA)
+val TextMuted = Color(0xFF4FB389)
+
+val ErrorRed = Color(0xFFFF5252)
